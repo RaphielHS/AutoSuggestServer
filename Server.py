96e1b552810a6fe9ab3243999d48904aaa8e40c7
RaphielHS <70839561+RaphielHS@users.noreply.github.com>
2022-04-20 20:28:58 +0700

    Add files via upload

--- Server.py
+++ Server.py
@@ -1,14 +1,102 @@
 # -*- coding: utf-8 -*-
+# NOTE: This is stil unstable
 
-import time
+__version__ = 'b0.0.2'
+
+import requests
 import os
+import time
 import json
 import logging
+import sqlite3
+import hashlib
+import base64
 
 from datetime import datetime
 from flask import Flask, request
 from flask_restful import Api, Resource
 from colorama import Style, Fore, init
+
+class Encryption:
+    def SHA256Encrypt(self, message) -> str:
+        return hashlib.sha256(message.encode()).hexdigest()
+
+    def Base64Encrypt(self, message) -> str:
+        return base64.b64encode(message.encode('ascii')).decode('ascii')
+
+    def Base64Decrypt(self, message) -> str:
+        return base64.b64decode(message.encode('ascii')).decode('ascii')
+
+    def CheckSamiliarity(self, str1, str2) -> dict:
+        resp = dict()
+        isSamiliar = False
+        if self.SHA256Encrypt(str1) == self.SHA256Encrypt(str2):
+            isSamiliar = True
+        resp[str1] = {"SHA256-hash": self.SHA256Encrypt(str1), "Base64-hash": self.Base64Encrypt(str1), "samiliar": isSamiliar}
+        resp[str2] = {"SHA256-hash": self.SHA256Encrypt(str2), "Base64-hash": self.Base64Encrypt(str2), "samiliar": isSamiliar}
+        return resp
+
+class Auth:
+    def __init__(self):
+        if not os.path.exists("./database/admin/users/"):
+            os.makedirs("./database/admin/users/")
+        db = sqlite3.connect("./database/admin/users/users.db")
+        cur = db.cursor()
+
+        with db:
+            cur.execute("""CREATE TABLE IF NOT EXISTS Admins(
+                ID INTEGER,
+                username REAL,
+                password REAL,
+                Lastused REAL
+            )""")
+
+    def GetPassword(self, username):
+        if not os.path.exists("./database/admin/users/"):
+            os.makedirs("./database/admin/users/")
+        db = sqlite3.connect("./database/admin/users/users.db")
+        cur = db.cursor()
+
+        with db:
+            cur.execute("""CREATE TABLE IF NOT EXISTS Admins(
+                ID INTEGER,
+                username REAL,
+                password REAL,
+                Lastused REAL
+            )""")
+        passws = cur.execute("SELECT password FROM Admins WHERE (username=username)", {"username": username}).fetchone()
+        try:
+            return passws[0]
+        except:
+            return ""
+
+    def ExecuteAdminQueryDatabase(self, query, params):
+        try:
+            if not os.path.exists("./database/admin/users/"):
+                os.makedirs("./database/admin/users/")
+            db = sqlite3.connect("./database/admin/users/users.db")
+            cur = db.cursor()
+
+            with db:
+                cur.execute("""CREATE TABLE IF NOT EXISTS Admins(
+                    ID INTEGER,
+                    username REAL,
+                    password REAL,
+                    Lastused REAL
+                )""")
+            with db:
+                cur.execute(query, param)
+            return True
+        except:
+            return False
+
+    def CheckUser(self, username, password) -> bool:
+        passw = self.GetPassword(username)
+        if passw != None or passw != " " or passw != "":
+            if passw == password:
+                return True
+        else:
+            return False
 
 class Logging:
     __isInit__ = False;
@@ -100,9 +188,7 @@ class JSONDatabase:
         Console().Log("Checking Database file")
         if not os.path.exists("./database/database.json"):
             Console().Log("Database File dosent exists, Creating Database file")
-            tempDt = {
-                "misc": "chicken"
-            }
+            tempDt = {}
             with open("./database/database.json", "w+") as f:
                 json.dump(tempDt, f, ensure_ascii=False, indent=4)
             Console().Log("Database file created")
@@ -139,33 +225,63 @@ class JSONDatabase:
                     Console().Error(f"Unknown Error has occured when trying check if the category {category} Exists, Err: {e}\n")
                     return None
         if all:
-            # NOTE: Debug thingy curl http://127.0.0.1:5000/get?q=chick
             Console().Log(f"The category Parameter is None, Reserting to all categories.")
-            # Console().Log(f"Getting Database contents")
             Console().Log(f"Getting Database contents and searching")
             DataList = list()
             resps = dict()
             for i in data:
                 for k in data[i]:
-                    # DataList.append(k)
                     if k.lower().__contains__(query.lower()) or k.lower().startswith(query.lower()):
                         if not i in resps:
                             resps[i] = []
                         resps[i].append(k)
-            # Console().Log(f"Got {len(DataList)} Variables. Searching")
-            # resp = list()
-            # for i in DataList:
-            #     if i.lower().__contains__(query.lower()) or i.lower().startswith(query.lower()):
-            #         resp.append(i)
-            # Console().Log(f"Returning {len(resp)} Results.")
             Console().Log(f"Returning {len(resps)} Results.\n")
             return resps
+
+class AdminControl:
+    def __init__(self):
+        if not os.path.exists("./database/admin/users/"):
+            os.makedirs("./database/admin/users/")
+        db = sqlite3.connect("./database/admin/users/users.db")
+        cur = db.cursor()
+
+        with db:
+            cur.execute("""CREATE TABLE IF NOT EXISTS Admins(
+                ID INTEGER,
+                username REAL,
+                password REAL,
+                Lastused REAL
+            )""")
+
+    def GetIDs(self):
+        db = sqlite3.connect("./database/admin/users/users.db")
+        cur = db.cursor()
+        return cur.execute("SELECT ID FROM Admins").fetchall()
+
+    def GetUsernames(self):
+        db = sqlite3.connect("./database/admin/users/users.db")
+        cur = db.cursor()
+        return cur.execute("SELECT username FROM Admins").fetchall()
+
+    def AddAdmin(self, username, rawPassword):
+        db = sqlite3.connect("./database/admin/users/users.db")
+        cur = db.cursor()
+        id = len(self.GetIDs())
+        for i in self.GetUsernames():
+            if i[0] == username:
+                return {"Message": "user already exists"}
+        with db:
+            cur.execute("INSERT INTO Admins(ID, username, password, Lastused) VALUES (?,?,?,?)",
+                (id + 1, username, Encryption().SHA256Encrypt(rawPassword), datetime.now()))
+            return {"Message": "added a new admin: " + username}
 
 class API_Endpoints:
     class Get(Resource):
         def get(self):
             query = request.args.get("q")
             cate = request.args.get("category")
+            if query == "" or query == None or query == " ":
+                return {"message": "Query is empty."}
             res = JSONDatabase().Get(query, cate)
             try:
                 res["suggestions"][cate]
@@ -175,11 +291,84 @@ class API_Endpoints:
                 cate = "all"
             return {"query": query, "suggestions": res, "category": cate}
 
-server = Flask(__name__)
-api = Api(server)
+    class AdminDatabase:
+        class AddToDatabase(Resource):
+            def post(self):
+                JSONDatabase()
+                try:
+                    data = json.loads(str(request.data.decode()))
+                    username = data["auth"]["cred"]["username"]
+                    passw = data["auth"]["cred"]["password"]
+                    Console().Log("Checking Admin Cred")
+                    if Auth().CheckUser(username, passw):
+                        dt = data["data"]
+                        cate = dt["category"]
+                        cateVal = dt["categoryValues"]
+                        duplicates = []
+                        with open("./database/database.json", 'r') as f:
+                            dts = json.load(f)
+                        Console().Log("Checking Category")
+                        try:
+                            dts[cate]
+                        except:
+                            dts[cate] = []
+                        Console().Log("Checking for duplicates")
+                        for i in dts[cate]:
+                            if isinstance(cateVal, list):
+                                for r in cateVal:
+                                    if i.lower() == r.lower():
+                                        cateVal.append(i)
+                                        break
+                            else:
+                                try:
+                                    if i.lower() == cateVal.lower():
+                                        Console().Warning("key [" + i + "] Already Exists")
+                                        return {"Message": "The key already exists in the database"}
+                                except Exception as e:
+                                    Console().Error("key [" + i + "] Unknown error, " + e)
+                                    return {"Error": "Unknown Error"}
+                        Console().Log("Checking Value Type")
+                        if isinstance(cateVal, list):
+                            for i in cateVal:
+                                if not duplicates.__contains__(cateVal):
+                                    Console().Log("Adding Value [" + i + "] to Category " + cate)
+                                    dts[cate].append(i)
+                                else:
+                                    Console().Warning("Duplicate [" + i + "] Found")
+                        else:
+                            Console().Log("Adding Value [" + cateVal + "] to Category " + cate)
+                            if not duplicates.__contains__(cateVal):
+                                dts[cate].append(cateVal)
+                            else:
+                                Console().Warning("Duplicate [" + i + "] Found")
+                        Console().Log("Saving Database and writing...\n")
+                        with open("./database/database.json", 'w') as f:
+                            json.dump(dts, f, indent=4, ensure_ascii=False)
+                        return {"Message": "Added Items Successfully"}
+                    return {"Message": "Invalid JSON data"}
+                except Exception as e:
+                    if type(e) == KeyError:
+                        return {"Message": "Invalid JSON Data"}
+                    return {"Message": e}
 
-api.add_resource(API_Endpoints.Get, "/get")
+def main():
+    server = Flask(__name__)
+    api = Api(server)
+
+    api.add_resource(API_Endpoints.Get, "/get")
+    api.add_resource(API_Endpoints.AdminDatabase.AddToDatabase, "/admin/database/add")
+
+    Console().Warning(f"{Fore.RED}THIS IS STILL UNSTABLE, I (RaphielHS) DO NOT TAKE RESPONSIBILITY FOR ANY DATA THAT IS LOST.")
+    time.sleep(3)
+    Console().Log("Checking for Updates")
+    r = requests.get("https://raw.githubusercontent.com/RaphielHS/AutoSuggestServer/main/__version__").text
+    if r != __version__:
+        Console().Warning(f"This Version of the server is Outdated. Please update at https://github.com/RaphielHS/AutoSuggestServer")
+        time.sleep(3)
+    else:
+        Console().Log(f"This Version of the server is up to date")
+
+    server.run(debug=True)
 
 if __name__ == '__main__':
-    # os.system("cls")
-    server.run(debug=True)
+    main()
